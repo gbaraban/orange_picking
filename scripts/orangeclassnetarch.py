normalization fix, gaussian fix, changes for newer versions of tf

scripts/orangeclassnetarch.py
@@ -68,20 +68,26 @@ def resnet8(img_input, num_pts, bins, scope='Prediction', reuse=False, f=0.25, r
         x = Activation('relu')(x)
         if kr is not None:
           x = Dropout(0.5)(x)
-        x = Dense(int(256*f))(x)
+        x = Dense(int(4096*f))(x)
         x = Activation('relu')(x)
         
-        for ii in range(dense):#Add more dense layers
-            x = Dense(int(256*f))(x)
-            x = Activation('relu')(x)
+        # for ii in range(dense):#Add more dense layers
+        x = Dense(int(2048*f))(x)
+        x = Activation('relu')(x)
 
+        x = Dense(int(1024*f))(x)
+        x = Activation('relu')(x)
+
+        
         # Output channel
         logits = []
+
         for ii in range(num_pts):
             temp_list = []
             for jj in range(3):
               temp = Dense(bins)(x)
               temp_list.append(temp)
+            
             logits.append(temp_list)
 
     return logits
@@ -108,8 +114,8 @@ class OrangeClassNet:
 
   def __init__(self, capacity = 1, num_img = 2, num_pts = 1, focal_l = -1, min_xyz = (0,-0.5,-0.5), max_xyz = (1,0.5,0.5), bins = 100, dense = 1):
     #Parameters
-    self.w = 300
-    self.h = 200
+    self.w = 640 #300
+    self.h = 380 #200 
     self.num_points = num_pts
     self.num_images = num_img
     self.f = capacity#5.0#2.0#1.5#125#1#0.25
@@ -131,14 +137,14 @@ class OrangeClassNet:
     self.losses = []#[tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.waypoint_output[ii], logits=self.logits[ii])) for ii in range(3)]
     for ii in range(self.num_points):
         for jj in range(3):
-            temp = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.waypoint_output[jj][:,ii,:], logits=self.logits[ii][jj]))
+            temp = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.waypoint_output[jj][:,ii,:], logits=self.logits[ii][jj]))
             self.losses.append(temp)
 
     #self.losses = [tf.nn.softmax_cross_entropy_with_logits(labels=self.waypoint_output[ii], logits=self.logits[ii]) for ii in range(3)]
     self.objective = sum(self.losses)
     #self.objective = tf.reduce_mean(self.losses[0] + self.losses[1] + self.losses[2])
     self.learning_fac = tf.Variable(self.learning_fac_init)
-    opt_op = tf.train.AdamOptimizer(self.learning_fac).minimize(self.objective)
+    opt_op = tf.compat.v1.train.AdamOptimizer(self.learning_fac).minimize(self.objective)
     self.train_step = opt_op
     #self.iterations = tf.Variable(0)
     #self.waypoint_list = []
@@ -149,4 +155,4 @@ class OrangeClassNet:
     self.val_summ = tf.summary.scalar('Validation Objective Function', self.objective)
     self.lf_summ = tf.summary.scalar('Learning Factor', self.learning_fac)
     #self.val_image_summ = tf.summary.image('Validation Image',self.generate_image)
-    self.merge = tf.summary.merge_all()
+    self.merge = tf.compat.v1.train.AdamOptimizer()

scripts/trainorangeclassnet.py
@@ -42,6 +42,8 @@ def parseDirData(run_dir, seed, resample, val_perc, num_pts, dt = 1):
   train_idx = num_samples - val_idx
   val_indices = rand_idx[-val_idx:]
   train_indices = rand_idx[:train_idx]
+  # print(train_indices)
+  # print(val_indices)
   return train_indices, val_indices
 
 def parseFiles(idx,traj_data,trial_dir, model):
@@ -53,7 +55,8 @@ def parseFiles(idx,traj_data,trial_dir, model):
     temp_image = img.open(trial_dir+'image'+str(temp_idx)+'.png').resize((model.w,model.h))
     temp_image = np.array(temp_image.getdata()).reshape(temp_image.size[0],temp_image.size[1],3)
     temp_image = temp_image[:,:,0:3]/255.0 #Cut out alpha
-    temp_image = temp_image/255.0
+    # temp_image = temp_image/255.0
+    
     if image is None:
         image = temp_image
     else:
@@ -80,18 +83,26 @@ def parseFiles(idx,traj_data,trial_dir, model):
     bin_nums = (point - model.min)/(model.max-model.min)
     bin_nums = (point*model.bins).astype(int)
     bin_nums = np.clip(bin_nums,a_min=0,a_max=model.bins-1)
+
     labels = np.zeros((3,model.bins))
-    for ii in range(3):
-      #Adding smoothing to labels
-      labels[ii,bin_nums[ii]] = 0.5
-      if bin_nums[ii] > 0:
-        labels[ii,bin_nums[ii]-1] = 0.25
-      else:
-        labels[ii,bin_nums[ii]] += 0.25
-      if bin_nums[ii] < (model.bins-1):
-        labels[ii,bin_nums[ii] + 1] = 0.25
-      else:
-        labels[ii,bin_nums[ii]] += 0.25
+    
+    mean = 1
+    stdev = 1
+    for j in range(len(bin_nums)):
+      for i in range(labels.shape[1]):
+        labels[j][i] = mean * (np.exp((-np.power(bin_nums[j]-i, 2))/(2 * np.power(stdev, 2))))
+
+    # for ii in range(3):
+    #   #Adding smoothing to labels
+    #   labels[ii,bin_nums[ii]] = 0.5
+    #   if bin_nums[ii] > 0:
+    #     labels[ii,bin_nums[ii]-1] = 0.25
+    #   else:
+    #     labels[ii,bin_nums[ii]] += 0.25
+    #   if bin_nums[ii] < (model.bins-1):
+    #     labels[ii,bin_nums[ii] + 1] = 0.25
+    #   else:
+    #     labels[ii,bin_nums[ii]] += 0.25
     local_pts.append(labels)
   local_pts = np.array(local_pts)
   local_pts.resize((model.num_points,3,model.bins))
@@ -100,6 +111,7 @@ def parseFiles(idx,traj_data,trial_dir, model):
 def loadData(idx,run_dir,model,dt = 1):
   num_points = model.num_points
   time_window = num_points*dt
+  # print(idx)
   #Assume reduced N is constant for all trials
   trial_list = os.listdir(run_dir)
   trial_dir = trial_list[0]
@@ -160,13 +172,13 @@ def main():
     os.environ["CUDA_VISIBLE_DEVICES"]=args.gpus
 
   # Model and optimization params
-  val_perc = 0.01
+  val_perc = 0.2
   #g_depths = [64, 64, 64]
   #f_depths = [64, 64, 64]
   batch_size = args.batch_size#512#1024#64
   num_epochs = args.epochs
   learning_rate = args.learning_rate#50#e-1
-  learn_rate_decay = 10 / num_epochs
+  learn_rate_decay = 500 / num_epochs
   save_variables_divider = 10
   log_path = './model/logs'
   save_path = createStampedFolder(os.path.join(log_path, 'variable_log'))
@@ -238,7 +250,7 @@ def main():
       # Decay learning rate
       new_learn_rate = np.exp(-epoch*learn_rate_decay)*learning_rate
       print('Learning Rate Set to: ' + str(new_learn_rate))
-      model.learning_fac.assign(new_learn_rate)
+      model.learning_fac.assign(new_learn_rate) 
       while batch_idx < num_train_samples:
         end_idx = min(batch_idx + batch_size, num_train_samples)
         train_inputs, train_outputs_x, train_outputs_y, train_outputs_z = loadData(train_indices[batch_idx:end_idx],args.data, model)