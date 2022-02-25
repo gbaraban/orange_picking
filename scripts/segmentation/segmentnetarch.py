f1d6ee64970198521fc811146ae83e9f9827e126
Gabriel Baraban <gbaraba1@jhu.edu>
2022-02-25 15:13:48 -0500

    cv_bridge pre-commit

--- scripts/segmentation/segmentnetarch.py
+++ scripts/segmentation/segmentnetarch.py
@@ -7,37 +7,40 @@ import numpy as np
 
 class SegmentationNet(torch.nn.Module):
     def __init__(self, capacity = 1, retrain_off_seg = False):
+        print("segInit")
         super(SegmentationNet,self).__init__()
         self.w = 640
         self.h = 480
         self.f = capacity
         self.n_classes = 2
-
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=128, kernel_size=10, stride=4, padding=5)
         self.bn1 = nn.BatchNorm2d(128)
         self.relu1 = nn.ReLU()
         self.conv2 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=2)
         self.bn2 = nn.BatchNorm2d(256)
         self.relu2 = nn.ReLU()
+        print(3)
         self.conv3 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=4, stride=2, padding=1)
+        print(2)
         self.bn3 = nn.BatchNorm2d(512)
+        print(2)
         self.relu3 = nn.ReLU()
+        print(4)
         self.conv4 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=1, stride=1)
-
         self.conv_transpose1 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=4, stride=2, padding=1)
         self.bn4 = nn.BatchNorm2d(256)
         self.relu4 = nn.ReLU()
-
         self.conv_transpose2 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=3, stride=2, padding=2)
+        print(5)
         self.bn5 = nn.BatchNorm2d(128)
         self.relu5 = nn.ReLU()
-
+        print("conv")
         self.conv_transpose3 = nn.ConvTranspose2d(in_channels=128, out_channels=self.n_classes, kernel_size=10, stride=4, padding=5)
         #print(self.conv_transpose2)
         self.conv_bridge = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=1, stride=1)
         self.conv_bridge2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=1, stride=1)
-
         self.output = nn.Conv2d(in_channels=2, out_channels=2, kernel_size=1, stride=1)
+        print("end init seg")
         if retrain_off_seg:
             print("Seg layers frozen")
             self.conv1.weight.requires_grad = False
@@ -46,47 +49,38 @@ class SegmentationNet(torch.nn.Module):
             self.bn1.bias.requires_grad = False
             #self.relu1.weight.requires_grad = False
             #self.relu1.bias.requires_grad = False
-
             self.conv2.weight.requires_grad = False
             self.conv2.bias.requires_grad = False
             self.bn2.weight.requires_grad = False
             self.bn2.bias.requires_grad = False
             #self.relu2.weight.requires_grad = False
             #self.relu2.bias.requires_grad = False
-
             self.conv3.weight.requires_grad = False
             self.conv3.bias.requires_grad = False
             self.bn3.weight.requires_grad = False
             self.bn3.bias.requires_grad = False
             #self.relu3.weight.requires_grad = False
             #self.relu3.bias.requires_grad = False
-
             self.conv4.weight.requires_grad = False
             self.conv4.bias.requires_grad = False
-
             self.conv_transpose1.weight.requires_grad = False
             self.conv_transpose1.bias.requires_grad = False
             self.bn4.weight.requires_grad = False
             self.bn4.bias.requires_grad = False
             #self.relu4.weight.requires_grad = False
             #self.relu4.bias.requires_grad = False
-
             self.conv_transpose2.weight.requires_grad = False
             self.conv_transpose2.bias.requires_grad = False
             self.bn5.weight.requires_grad = False
             self.bn5.bias.requires_grad = False
             #self.relu5.weight.requires_grad = False
             #self.relu5.bias.requires_grad = False
-
             self.conv_transpose3.weight.requires_grad = False
             self.conv_transpose3.bias.requires_grad = False
-
             self.conv_bridge.weight.requires_grad = False
             self.conv_bridge.bias.requires_grad = False
-
             self.conv_bridge2.weight.requires_grad = False
             self.conv_bridge2.bias.requires_grad = False
-
             self.output.weight.requires_grad = False
             self.output.bias.requires_grad = False
 
